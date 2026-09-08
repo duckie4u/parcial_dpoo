f34733c9c45b09d5439067f44bc82851e1b34308
duckie4u <dev@capitalsbpo.com>
2026-09-08 08:43:55 -0500

    cambio de float a double

--- src/logic/Mujer.java
+++ src/logic/Mujer.java
@@ -1,16 +1,28 @@
 package logic;
 
 public class Mujer extends Persona {
-	public Mujer (float peso, float altura, int edad) {
+	public Mujer (double peso, double altura, int edad) {
 		super(peso, altura, edad);
 	}
-	
+
 	@Override
-	public abstract float calcularTBM (float peso, float altura, int edad) throws EdadOutOfRange, AltruraOutOfRange, PesoOutOfRange{
+	public float calcularTBM(double peso, double altura, int edad)
+			throws EdadOutOfRange, AltruraOutOfRange, PesoOutOfRange {
 		if (peso < 40.0 || peso > 110.0) {
 			throw new PesoOutOfRange("Error: PesoOutOfRange");
 		}
-		if (altura < 160.0 || altura > 195.0)
+		if (altura < 160.0 || altura > 195.0) {
+			throw new AltruraOutOfRange("Error: AlturaOutOfRange");
+		}
+		if (edad <= 15) {
+			throw new EdadOutOfRange("Error: EdadOutOfRange");
+		}
+		
+		float tbm  = 447.593 + (9.247 * peso) + (3.098 * altura) - (4.33 * edad);
+		return tbm;
 			
 	}
-}
+	
+	
+	}
+
--- src/logic/Persona.java
+++ src/logic/Persona.java
@@ -1,14 +1,14 @@
 package logic;
 
 public abstract class Persona {
-	protected float peso;
-	protected float altura;
+	protected double peso;
+	protected double altura;
 	protected int edad;
 	
-	public Persona (float peso_kg, float altura_cm, int edad_anios) {
+	public Persona (double peso_kg, double altura_cm, int edad_anios) {
 		this.peso = peso_kg;
 		this.altura = altura_cm;
 		this.edad = edad_anios;
 	}
-	public abstract float calcularTBM (float peso, float altura, int edad);
+	public abstract float calcularTBM (double peso, double altura, int edad) throws EdadOutOfRange, AltruraOutOfRange, PesoOutOfRange;
 }
