bfc0c17f0941b1f8d1875849187c1e7a328481af
duckie4u <dev@capitalsbpo.com>
2026-09-08 08:21:51 -0500

    añadiendo clases

---
+++ src/logic/Persona.java
@@ -0,0 +1,14 @@
+package logic;
+
+public abstract class Persona {
+	protected float peso;
+	protected float altura;
+	protected int edad;
+	
+	public Persona (float peso_kg, float altura_cm, int edad_anios) {
+		this.peso = peso_kg;
+		this.altura = altura_cm;
+		this.edad = edad_anios;
+	}
+	public abstract double calcularTBM (float peso, float altura, int edad);
+}
